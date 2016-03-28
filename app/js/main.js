fbd85f26b36c65b84bc2c71d179a05704b948b53
Leisan <allarepossible@gmail.com>
2016-03-28 18:46:47 +0300

    Save list of friends using Local Storage

--- app/js/main.js
+++ app/js/main.js
@@ -1,15 +1,17 @@
 function allowDrop(ev) {
     ev.preventDefault();
 }
+
 function drag(ev) {
     ev.dataTransfer.setData("text", ev.target.id);
 }
+
 function drop(ev) {
     ev.preventDefault();
     var data = ev.dataTransfer.getData("text");
     if (ev.target == friendList2 || ev.target == friendList) {
         ev.target.insertBefore(document.getElementById(data), ev.target.firstChild);
-    } else if (ev.target.parentNode.parentNode == results || ev.target.parentNode.parentNode.parentNode == results || ev.target.parentNode.parentNode.parentNode.parentNode  == results ){
+    } else if (ev.target.parentNode.parentNode == results || ev.target.parentNode.parentNode.parentNode == results || ev.target.parentNode.parentNode.parentNode.parentNode == results) {
         friendList.insertBefore(document.getElementById(data), friendList.firstChild);
     } else {
         friendList2.insertBefore(document.getElementById(data), friendList2.firstChild);
@@ -18,22 +20,24 @@ function drop(ev) {
     check(friendList.children, first.value);
 }
 
-function toggleList(e){
-    if(e.target.matches('i')){
+function toggleList(e) {
+    if (e.target.matches('i')) {
         var data = e.target.parentNode.cloneNode('true');
-        friendList2.insertBefore(data,friendList2.firstChild);
+        friendList2.insertBefore(data, friendList2.firstChild);
         friendList.removeChild(e.target.parentNode);
     }
     check(friendList2.children, second.value);
 }
-function toggleList2(e){
-    if(e.target.matches('i')){
+
+function toggleList2(e) {
+    if (e.target.matches('i')) {
         var data = e.target.parentNode.cloneNode('true');
         friendList.insertBefore(data, friendList.firstChild);
         friendList2.removeChild(e.target.parentNode);
     }
     check(friendList.children, first.value);
 }
+
 results.onclick = toggleList;
 results2.onclick = toggleList2;
 
@@ -46,15 +50,27 @@ second.addEventListener('keyup', function () {
     check(friendList2.children, second.value);
 });
 
- function check(array, char){
-     for(var i=0; i< array.length; i++){
-         if(array[i].children[1].innerText.toLowerCase().indexOf(char)>=0){
-             array[i].style.display ='block';
-         } else {
-             array[i].style.display ='none';}
-     }
-     if(char == ''){
-         for(var i=0; i< array.length; i++)
-             array[i].style.display ='block';
-     }
- }
+function check(array, char) {
+    for (var i = 0; i < array.length; i++) {
+        if (array[i].children[1].innerText.toLowerCase().indexOf(char) >= 0) {
+            array[i].style.display = 'block';
+        } else {
+            array[i].style.display = 'none';
+        }
+    }
+    if (char == '') {
+        for (var i = 0; i < array.length; i++)
+            array[i].style.display = 'block';
+    }
+}
+
+save.onclick = function () {
+    if (friendList2.children.length > 0) {
+        var friendsArray = [];
+        for (var i = 0; i < friendList2.children.length; i++)
+            friendsArray.push(friendList2.children[i].getAttribute('id'));
+        localStorage.setItem('friendsArray', friendsArray);
+    } else {
+        localStorage.clear();
+    }
+}
--- app/js/script.js
+++ app/js/script.js
@@ -1,17 +1,17 @@
-new Promise(function(resolve) {
+new Promise(function (resolve) {
     if (document.readyState === 'complete') {
         resolve();
     } else {
         window.onload = resolve;
     }
 })
-    .then(function() {
-        return new Promise(function(resolve, reject) {
+    .then(function () {
+        return new Promise(function (resolve, reject) {
             VK.init({
                 apiId: 5373245
             });
 
-            VK.Auth.login(function(response) {
+            VK.Auth.login(function (response) {
                 if (response.session) {
                     resolve(response);
                 } else {
@@ -20,33 +20,34 @@ new Promise(function(resolve) {
             }, 2);
         });
     })
-    .then(function() {
-        return new Promise(function(resolve, reject) {
-            VK.api('users.get', {'name_case': 'gen'}, function(response) {
+    .then(function () {
+        return new Promise(function (resolve, reject) {
+            VK.api('friends.get', {fields: 'first_name,last_name,photo,user_id'}, function (response) {
                 if (response.error) {
                     reject(new Error(response.error.error_msg));
                 } else {
+                    var source = friendItemTemplate.innerHTML,
+                        templateFn = Handlebars.compile(source),
+                        template = templateFn({list: response.response});
+                    results.innerHTML = template;
+
+                    if (localStorage.friendsArray != undefined) {
+                        var array = localStorage.friendsArray.split(',');
+                        for (var i = 0; i < array.length; i++) {
+                            for (var j = 0; j < friendList.children.length; j++) {
+                                if (friendList.children[j].getAttribute('id') == array[i]) {
+                                    var data = friendList.children[j].cloneNode('true');
+                                    friendList2.appendChild(data);
+                                    friendList.removeChild(friendList.children[j]);
+                                }
+                            }
+                        }
+                    }
                     resolve();
                 }
             });
-        })
-    }).then(function() {
-    return new Promise(function (resolve, reject) {
-        VK.api('friends.get', {fields: 'first_name,last_name,photo,user_id'}, function (response) {
-            if (response.error) {
-                reject(new Error(response.error.error_msg));
-            } else {
-                var source = friendItemTemplate.innerHTML,
-                    templateFn = Handlebars.compile(source),
-                    template = templateFn({list: response.response});
-
-                results.innerHTML = template;
-
-                resolve();
-            }
         });
-    });
-}).catch(function(e) {
+    }).catch(function (e) {
     alert('Ошибка: ' + e.message);
 });
 
